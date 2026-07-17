1af0f5ccda161b20f70e8105a086d9773adb2db9
NyanLinHtet <ln25367020@ga.ttc.ac.jp>
2026-07-17 15:37:54 +0900

    Fix_triangle_loop

--- C_function_training/day03_C/triangle.c
+++ C_function_training/day03_C/triangle.c
@@ -11,15 +11,22 @@ void triangle(int n)
     }
 }
 int main(void){
-    int n = 1;
-    while (n!= 0)
+    int n;
+    while(1)
     {
+        printf("Enter Height 0r enter 0 to exit:");
+        printf("\n");
+        scanf("%d", &n);
 
-    printf("Enter height:  ");
-    scanf("%d", &n);
-
-    triangle(n);
-    printf("To stop enter 0...");
+        if(n == 0){
+            printf("See You later...\n");
+            break;
+        }
+        if(n < 0){
+            printf("Please Enter positive number(+)!\n");
+            continue;;
+        }
+        triangle(n);
     }
 
 
